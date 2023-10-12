73766d2b6fd93b223028890c8703c9cb1010c1a2
simbrn <83414209+simbrn@users.noreply.github.com>
2023-10-12 18:52:12 +0300

    test: 🚨 add player service test

--- test/jest.spec.ts
+++ test/jest.spec.ts
@@ -1,5 +1,74 @@
-describe('Tesing JEST framework', () => {
-  test('It works', () => {
-    expect(1).toEqual(1);
-  });
-});
+import { Test } from '@nestjs/testing';
+import { factory } from "../src/framework/provider";
+import { PlayerServiceImpl } from "../src/domain/services/player.service";
+import { PlayerRepositoryImpl } from "../src/infrastructure/persistence/repositories/player";
+import { Player } from '../src/domain/models/player.model';
+import { ConfigModule } from '@nestjs/config';
+import { CONFIG } from '../src/config';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { PlayerEntity } from '../src/infrastructure/persistence/entities/player';
+
+describe('PlayerService', () => {
+  let playerServiceImpl: PlayerServiceImpl
+  let playerRepositoryImpl: PlayerRepositoryImpl
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot({ load: Object.values(CONFIG), isGlobal: true }),
+        MikroOrmModule.forRoot({
+          contextName: 'TEST',
+          dbName: ':memory:',
+          type: 'postgresql',
+          entities: ['dist/infrastructure/persistence/entities/*.js'],
+          entitiesTs: ['src/infrastructure/persistence/entities/*.ts'],
+          allowGlobalContext: false,
+          forceUndefined: true,
+          debug: false,
+        }),
+        MikroOrmModule.forFeature({ entities: [PlayerEntity], contextName: 'TEST' }),
+        MikroOrmModule.forMiddleware(),
+      ],
+      providers: [factory(PlayerServiceImpl, [PlayerRepositoryImpl])],
+    }).compile();
+
+    playerServiceImpl = moduleRef.get<PlayerServiceImpl>(PlayerServiceImpl);
+    playerRepositoryImpl = moduleRef.get<PlayerRepositoryImpl>(PlayerRepositoryImpl);
+  })
+
+  describe('getPlayer', () => {
+    test('should return a player', async () => {
+      const result: Player = {
+        id: "H4Sh",
+        name: "Simas"
+      };
+      jest.spyOn(playerRepositoryImpl, 'getOneById').mockImplementation(async () => result);
+
+      expect(await playerServiceImpl.getPlayer(result.id)).toBe(result);
+    });
+  })
+
+  describe('createPlayer', () => {
+    test('should create a player', async () => {
+      const result: Player = {
+        id: "H4Sh",
+        name: "Simas"
+      };
+      jest.spyOn(playerRepositoryImpl, 'createOne').mockImplementation(async () => result);
+
+      expect(await playerServiceImpl.createPlayer(result.name)).toBe(result);
+    });
+  })
+
+  describe('deletePlayer', () => {
+    test('should delete a player', async () => {
+      const result: Player = {
+        id: "H4Sh",
+        name: "Simas"
+      };
+      const deleteSpy = jest.spyOn(playerRepositoryImpl, 'deleteOne').mockImplementation();
+      await playerServiceImpl.deletePlayer(result.id)
+      expect(deleteSpy).toBeCalledTimes(1);
+    });
+  })
+})
