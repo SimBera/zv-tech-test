7e90da15dfb16a1fb3ba92c0225e2a408257e00a
simbrn <83414209+simbrn@users.noreply.github.com>
2023-10-13 10:30:03 +0300

    chore: 🔧 lint-fix

--- test/jest.spec.ts
+++ test/jest.spec.ts
@@ -1,16 +1,17 @@
-import { Test } from '@nestjs/testing';
-import { factory } from "../src/framework/provider";
-import { PlayerServiceImpl } from "../src/domain/services/player.service";
-import { PlayerRepositoryImpl } from "../src/infrastructure/persistence/repositories/player";
-import { Player } from '../src/domain/models/player.model';
-import { ConfigModule } from '@nestjs/config';
-import { CONFIG } from '../src/config';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { ConfigModule } from '@nestjs/config';
+import { Test } from '@nestjs/testing';
+
+import { CONFIG } from '../src/config';
+import { Player } from '../src/domain/models/player.model';
+import { PlayerServiceImpl } from '../src/domain/services/player.service';
+import { factory } from '../src/framework/provider';
 import { PlayerEntity } from '../src/infrastructure/persistence/entities/player';
+import { PlayerRepositoryImpl } from '../src/infrastructure/persistence/repositories/player';
 
 describe('PlayerService', () => {
-  let playerServiceImpl: PlayerServiceImpl
-  let playerRepositoryImpl: PlayerRepositoryImpl
+  let playerServiceImpl: PlayerServiceImpl;
+  let playerRepositoryImpl: PlayerRepositoryImpl;
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
@@ -34,41 +35,41 @@ describe('PlayerService', () => {
 
     playerServiceImpl = moduleRef.get<PlayerServiceImpl>(PlayerServiceImpl);
     playerRepositoryImpl = moduleRef.get<PlayerRepositoryImpl>(PlayerRepositoryImpl);
-  })
+  });
 
   describe('getPlayer', () => {
     test('should return a player', async () => {
       const result: Player = {
-        id: "H4Sh",
-        name: "Simas"
+        id: 'H4Sh',
+        name: 'Simas',
       };
       jest.spyOn(playerRepositoryImpl, 'getOneById').mockImplementation(async () => result);
 
       expect(await playerServiceImpl.getPlayer(result.id)).toBe(result);
     });
-  })
+  });
 
   describe('createPlayer', () => {
     test('should create a player', async () => {
       const result: Player = {
-        id: "H4Sh",
-        name: "Simas"
+        id: 'H4Sh',
+        name: 'Simas',
       };
       jest.spyOn(playerRepositoryImpl, 'createOne').mockImplementation(async () => result);
 
       expect(await playerServiceImpl.createPlayer(result.name)).toBe(result);
     });
-  })
+  });
 
   describe('deletePlayer', () => {
     test('should delete a player', async () => {
       const result: Player = {
-        id: "H4Sh",
-        name: "Simas"
+        id: 'H4Sh',
+        name: 'Simas',
       };
       const deleteSpy = jest.spyOn(playerRepositoryImpl, 'deleteOne').mockImplementation();
-      await playerServiceImpl.deletePlayer(result.id)
-      expect(deleteSpy).toBeCalledTimes(1);
+      await playerServiceImpl.deletePlayer(result.id);
+      expect(deleteSpy).toHaveBeenCalledTimes(1);
     });
-  })
-})
+  });
+});
